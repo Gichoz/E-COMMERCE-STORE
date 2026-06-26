75729992d9ddc8e07c83e53b06c9204e0aa630af
Gichoz <kelvingichohi50@gmail.com>
2026-06-26 04:29:43 -0700

    refactor: move middleware setup to the top of server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
+// Middlewares
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 import express from "express";
 import {connectDB} from "./config/db.js";
 import mongoose from "mongoose";
@@ -23,11 +29,7 @@ const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middlewares
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 
 
 // Routes
